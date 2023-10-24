c1a97d5d6df660dec451397338f16b376f150a86
camilolcoder <j.colmenares@uniandes.edu.co>
2023-10-24 14:37:33 -0500

    un mini comentario

--- DataModel.py
+++ DataModel.py
@@ -5,6 +5,9 @@ class DataModel(BaseModel):
 
 # Estas varibles permiten que la librería pydantic haga el parseo entre el Json recibido y el modelo declarado.
     Textos_espanol: List[str]
+    #Textos_espanol: List[dict], este es un cambio que se podría discutir en equipo
+                    # pero siento que no es necesario ya que no afecta la implementación
+
 
 #Esta función retorna los nombres de las columnas correspondientes con el modelo esxportado en joblib.
     def columns(self):
