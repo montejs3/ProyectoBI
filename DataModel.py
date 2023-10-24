7e0626041671020130ea1b8b55a668bacf8fe5ed
montejs3 <98852422+montejs3@users.noreply.github.com>
2023-10-23 19:03:14 -0500

    Melo Caramelo

--- DataModel.py
+++ DataModel.py
@@ -6,8 +6,6 @@ class DataModel(BaseModel):
 # Estas varibles permiten que la librería pydantic haga el parseo entre el Json recibido y el modelo declarado.
     Textos_espanol: List[str]
 
-
-
 #Esta función retorna los nombres de las columnas correspondientes con el modelo esxportado en joblib.
     def columns(self):
         return ["Textos_espanol"]
--- main.py
+++ main.py
@@ -12,15 +12,6 @@ from joblib import load
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-   return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-   return {"item_id": item_id, "q": q}
-
 @app.post("/predict")
 def make_predictions(dataModel: DataModel):
     # df = pd.DataFrame(dataModel.model_dump(), columns=dataModel.model_dump().keys(), index=[0])
@@ -28,13 +19,12 @@ def make_predictions(dataModel: DataModel):
     # model = load("pipe.joblib")
     # result = model.predict(df)
     # return result
-
-    model_dump = dataModel.model_dump()
+    model_dump = dataModel.dict()
     df = pd.DataFrame({"Textos_espanol": model_dump["Textos_espanol"]})
     df.to_csv("test_como_queda.csv", sep=",", index=False, encoding='utf-8')
-    model = load("pipe.joblib")
-    result = model.predict(df["Textos_espanol"])
-    return result
+    model = load('pipe.joblib')
+    df['sdg'] = model.predict(df["Textos_espanol"])
+    return df.to_dict(orient="records")
 
 @app.post("/test_error") # El re hpstastaasdt error era que no tenia el formato en JSONNNNNNN
 def test_endpoint(data: DataModel):
