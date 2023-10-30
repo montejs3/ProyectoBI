cc2bfa5371752487bcf92e288c6191304cd50ac0
montejs3 <98852422+montejs3@users.noreply.github.com>
2023-10-29 20:59:30 -0500

    FInal

--- main.py
+++ main.py
@@ -37,21 +37,19 @@ app.add_middleware(
     allow_headers=["*"],  # You can specify specific headers if needed
 )
 
+
+
 @app.post("/predict")
 def make_predictions(dataModel: DataModel):
-    # df = pd.DataFrame(dataModel.model_dump(), columns=dataModel.model_dump().keys(), index=[0])
-    # df.columns = dataModel.columns()
-    # model = load("pipe.joblib")
-    # result = model.predict(df)
-    # return result
     model_dump = dataModel.dict()
     df = pd.DataFrame({"Textos_espanol": model_dump["Textos_espanol"]})
-
-    #Probar eliminiar la linea siguiente ->
-    df.to_csv("test_como_queda.csv", sep=",", index=False, encoding='utf-8')
+    df.to_csv("logEntrada.csv", sep=",", index=False, encoding='utf-8')
     model = load('pipe.joblib')
     df['sdg'] = model.predict(df["Textos_espanol"])
     return df.to_dict(orient="records")
+
+
+
 
 @app.post("/test_error") # El re hpstastaasdt error era que no tenia el formato en JSONNNNNNN
 def test_endpoint(data: DataModel):
